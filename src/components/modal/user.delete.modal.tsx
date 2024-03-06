500490564adaa1c012913fbed5d79c6823197fac
levbui <lev.bui@avepoint.com>
2024-03-06 16:27:44 +0700

    done handle edit and delete user

--- src/components/modal/user.delete.modal.tsx
+++ src/components/modal/user.delete.modal.tsx
@@ -1,37 +1,65 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Spinner } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { toast } from 'react-toastify';
 
 const UserDeleteModal = (props: any) => {
-    const { dataUser, isOpenDeleteModal, setIsOpenDeleteModal } = props;
+  const { dataUser, isOpenDeleteModal, setIsOpenDeleteModal } = props;
 
-    const handleSubmit = () => {
-        console.log({ id: dataUser?.id });
-    }
+  const queryClient = useQueryClient();
 
-    return (
-        <Modal
-            show={isOpenDeleteModal}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            backdrop={false}
-            onHide={() => setIsOpenDeleteModal(false)}
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>
-                    Delete A User
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                Delete the user: {dataUser?.email ?? ""}
-            </Modal.Body>
-            <Modal.Footer>
-                <Button
-                    variant='warning'
-                    onClick={() => setIsOpenDeleteModal(false)} className='mr-2'>Cancel</Button>
-                <Button onClick={() => handleSubmit()}>Confirm</Button>
-            </Modal.Footer>
-        </Modal>
-    )
-}
+  const mutation = useMutation({
+    mutationFn: async (payload: any) => {
+      const res = await fetch(`http://localhost:8000/users/${payload.id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': ' application/json',
+        },
+      });
+
+      return res.json();
+    },
+    onSuccess: () => {
+      toast('🦄 Wow so easy!');
+      setIsOpenDeleteModal(false);
+      queryClient.invalidateQueries({ queryKey: ['fetchUsers'] });
+    },
+  });
+
+  const handleSubmit = () => {
+    mutation.mutate({ id: dataUser?.id });
+  };
+
+  return (
+    <Modal
+      show={isOpenDeleteModal}
+      size='lg'
+      aria-labelledby='contained-modal-title-vcenter'
+      backdrop={false}
+      onHide={() => setIsOpenDeleteModal(false)}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Delete A User</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Delete the user: {dataUser?.email ?? ''}</Modal.Body>
+      <Modal.Footer>
+        {!mutation.isPending ? (
+          <>
+            <Button variant='warning' onClick={() => setIsOpenDeleteModal(false)} className='mr-2'>
+              Cancel
+            </Button>
+            <Button onClick={() => handleSubmit()}>Confirm</Button>
+          </>
+        ) : (
+          <Button variant='primary' disabled>
+            <Spinner as='span' animation='border' size='sm' role='status' aria-hidden='true' />
+            <></>Loading...
+          </Button>
+        )}
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 export default UserDeleteModal;
--- src/components/modal/user.edit.modal.tsx
+++ src/components/modal/user.edit.modal.tsx
@@ -3,77 +3,108 @@ import Modal from 'react-bootstrap/Modal';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { useState, useEffect } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import { Spinner } from 'react-bootstrap';
+
+// interface IUser {
+//   id: number;
+//   name: string;
+//   email: string;
+// }
 
 const UserEditModal = (props: any) => {
-    const { isOpenUpdateModal, setIsOpenUpdateModal, dataUser } = props;
-    const [id, setId] = useState();
+  const { isOpenUpdateModal, setIsOpenUpdateModal, dataUser } = props;
+  const [id, setId] = useState();
 
-    const [email, setEmail] = useState<string>("");
-    const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>('');
+  const [name, setName] = useState<string>('');
 
-    useEffect(() => {
-        if (dataUser?.id) {
-            setId(dataUser?.id);
-            setEmail(dataUser?.email);
-            setName(dataUser?.name)
-        }
-    }, [dataUser])
+  const queryClient = useQueryClient();
 
-
-    const handleSubmit = () => {
-        if (!email) {
-            alert("email empty");
-            return;
-        }
-        if (!name) {
-            alert("name empty");
-            return;
-        }
-        console.log({ email, name, id })
+  useEffect(() => {
+    if (dataUser?.id) {
+      setId(dataUser?.id);
+      setEmail(dataUser?.email);
+      setName(dataUser?.name);
     }
+  }, [dataUser]);
 
-    return (
-        <>
-            <Modal
-                show={isOpenUpdateModal}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                backdrop={false}
-                onHide={() => setIsOpenUpdateModal(false)}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>
-                        Update A User
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <FloatingLabel
-                        label="Email"
-                        className="mb-3"
-                    >
-                        <Form.Control
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="text"
-                        />
-                    </FloatingLabel>
-                    <FloatingLabel label="Name">
-                        <Form.Control
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            type="text"
-                        />
-                    </FloatingLabel>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button
-                        variant='warning'
-                        onClick={() => setIsOpenUpdateModal(false)} className='mr-2'>Cancel</Button>
-                    <Button onClick={() => handleSubmit()}>Confirm</Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    )
-}
+  const mutation = useMutation({
+    mutationFn: async (payload: any) => {
+      const res = await fetch(`http://localhost:8000/users/${payload.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          email: payload.email,
+          name: payload.name,
+        }),
+        headers: {
+          'Content-Type': ' application/json',
+        },
+      });
+      return res.json();
+    },
+    onSuccess: () => {
+      toast('🦄 Wow so easy!');
+      setIsOpenUpdateModal(false);
+      queryClient.invalidateQueries({ queryKey: ['fetchUsers'] });
+    },
+  });
+
+  const handleSubmit = () => {
+    if (!email) {
+      alert('email empty');
+      return;
+    }
+    if (!name) {
+      alert('name empty');
+      return;
+    }
+    mutation.mutate({ email, name, id });
+  };
+
+  return (
+    <>
+      <Modal
+        show={isOpenUpdateModal}
+        size='lg'
+        aria-labelledby='contained-modal-title-vcenter'
+        backdrop={false}
+        onHide={() => setIsOpenUpdateModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update A User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FloatingLabel label='Email' className='mb-3'>
+            <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type='text' />
+          </FloatingLabel>
+          <FloatingLabel label='Name'>
+            <Form.Control value={name} onChange={(e) => setName(e.target.value)} type='text' />
+          </FloatingLabel>
+        </Modal.Body>
+        <Modal.Footer>
+          {!mutation.isPending ? (
+            <>
+              <Button
+                variant='warning'
+                onClick={() => setIsOpenUpdateModal(false)}
+                className='mr-2'
+              >
+                Cancel
+              </Button>
+              <Button onClick={() => handleSubmit()}>Confirm</Button>
+            </>
+          ) : (
+            <Button variant='primary' disabled>
+              <Spinner as='span' animation='border' size='sm' role='status' aria-hidden='true' />
+              <></>Loading...
+            </Button>
+          )}
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
 export default UserEditModal;
