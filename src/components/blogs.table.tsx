989e2b048d68ba6907074f8aad237564ce1899b4
Viet-Truong <viettruong0825@gmail.com>
2024-03-15 11:17:23 +0700

    done blogs CRUD

--- src/components/blogs.table.tsx
+++ src/components/blogs.table.tsx
@@ -1,129 +1,169 @@
-
 import Table from 'react-bootstrap/Table';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, forwardRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import BlogCreateModal from './modal/blog.create.modal';
 import BlogEditModal from './modal/blog.edit.modal';
 import BlogDeleteModal from './modal/blog.delete.modal';
+import UsersPagination from './pagination/users.pagination';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { calculatePagesCount } from '../helper';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
 
+interface IBlog {
+  id: number;
+  title: string;
+  author: string;
+  content: string;
+}
+
+const PAGE_SIZE = 5;
 
 function BlogsTable() {
+  const [isOpenCreateModal, setIsOpenCreateModal] = useState<boolean>(false);
+  const [isOpenUpdateModal, setIsOpenUpdateModal] = useState<boolean>(false);
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState<boolean>(false);
 
+  const [dataBlog, setDataBlog] = useState({});
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPage, setTotalPage] = useState<number>(1);
 
-    const [isOpenCreateModal, setIsOpenCreateModal] = useState<boolean>(false);
+  const {
+    isPending,
+    error,
+    data: blogs,
+  } = useQuery({
+    queryKey: ['fetchBlogs', currentPage],
+    queryFn: (): Promise<IBlog[]> =>
+      fetch(`http://localhost:8000/blogs?_page=${currentPage}&_limit=${PAGE_SIZE}`).then((res) => {
+        const total_items = +(res.headers?.get('X-Total-Count') ?? 0);
+        const page_size = PAGE_SIZE;
+        setTotalPage(calculatePagesCount(page_size, total_items));
+        return res.json();
+      }),
+    placeholderData: keepPreviousData,
+  });
 
-    const [isOpenUpdateModal, setIsOpenUpdateModal] = useState<boolean>(false);
-    const [dataBlog, setDataBlog] = useState({});
+  if (isPending) return 'Loading...';
 
-    const [isOpenDeleteModal, setIsOpenDeleteModal] = useState<boolean>(false);
-    const blogs = [
-        {
-            "id": 1,
-            "title": "What is Lorem Ipsum?",
-            "author": "Hỏi Dân IT",
-            "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        },
-        {
-            "id": 2,
-            "title": "Why do we use it?",
-            "author": "Hỏi Dân IT",
-            "content": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-        },
-        {
-            "id": 3,
-            "title": "Where does it come from?",
-            "author": "Hỏi Dân IT",
-            "content": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,comes from a line in section 1.10.32."
-        },
-        {
-            "id": 4,
-            "title": "Why do we use it?",
-            "author": "Hỏi Dân IT",
-            "content": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-        },
-        {
-            "id": 5,
-            "title": "Where can I get some?",
-            "author": "Hỏi Dân IT",
-            "content": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        }
-    ]
+  if (error) return 'An error has occurred: ' + error.message;
 
-    const handleEditBlog = (blog: any) => {
-        setDataBlog(blog);
-        setIsOpenUpdateModal(true);
-    }
+  const handleEditBlog = (blog: any) => {
+    setDataBlog(blog);
+    setIsOpenUpdateModal(true);
+  };
 
-    const handleDelete = (blog: any) => {
-        setDataBlog(blog);
-        setIsOpenDeleteModal(true);
-    }
+  const handleDelete = (blog: any) => {
+    setDataBlog(blog);
+    setIsOpenDeleteModal(true);
+  };
+
+  const PopoverComponent = forwardRef((props: any, ref: any) => {
+    const { id } = props;
+
+    const {
+      isPending,
+      error,
+      data: blog,
+    } = useQuery({
+      queryKey: ['fetchBlog', id],
+      queryFn: (): Promise<IBlog> =>
+        fetch(`http://localhost:8000/blogs/${id}`).then((res) => res.json()),
+    });
+
+    const getBody = () => {
+      if (isPending) return 'Loading detail ...';
+      if (error) return 'An error has occurred: ' + error.message;
+      if (blog) {
+        return (
+          <>
+            <div>ID = {id}</div>
+            <div>Title = {blog?.title}</div>
+            <div>Author = {blog?.author}</div>
+          </>
+        );
+      }
+    };
 
     return (
-        <>
-            <div style={{ display: "flex", justifyContent: "space-between", margin: "15px 0" }}>
-                <h4>Table Blogs</h4>
-                <Button variant="primary"
-                    onClick={() => setIsOpenCreateModal(true)}
-                >Add New</Button>
-            </div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Title</th>
-                        <th>Author</th>
-                        <th>Content</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {blogs?.map(blog => {
-                        return (
-                            <tr key={blog.id}>
-                                <td>{blog.id}</td>
-                                <td>{blog.title}</td>
-                                <td>{blog.author}</td>
-                                <td>{blog.content}</td>
-
-                                <td>
-                                    <Button
-                                        variant="warning"
-                                        onClick={() => handleEditBlog(blog)}
-                                        className='mb-3'
-                                    >
-                                        Edit
-                                    </Button>&nbsp;&nbsp;&nbsp;
-                                    <Button
-                                        variant="danger"
-                                        onClick={() => handleDelete(blog)}
-                                    >
-                                        Delete
-                                    </Button>
-                                </td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </Table>
-
-            <BlogCreateModal
-                isOpenCreateModal={isOpenCreateModal}
-                setIsOpenCreateModal={setIsOpenCreateModal}
-            />
-
-            <BlogEditModal
-                isOpenUpdateModal={isOpenUpdateModal}
-                setIsOpenUpdateModal={setIsOpenUpdateModal}
-                dataBlog={dataBlog}
-            />
-
-            <BlogDeleteModal
-                dataBlog={dataBlog}
-                isOpenDeleteModal={isOpenDeleteModal}
-                setIsOpenDeleteModal={setIsOpenDeleteModal}
-            />
-        </>
+      <Popover ref={ref} {...props}>
+        <Popover.Header as='h3'>Detail User</Popover.Header>
+        <Popover.Body>{getBody()}</Popover.Body>
+      </Popover>
     );
+  });
+
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'space-between', margin: '15px 0' }}>
+        <h4>Table Blogs</h4>
+        <Button variant='primary' onClick={() => setIsOpenCreateModal(true)}>
+          Add New
+        </Button>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>
+          {blogs?.map((blog) => {
+            return (
+              <tr key={blog.id}>
+                <OverlayTrigger
+                  trigger='click'
+                  placement='right'
+                  rootClose
+                  overlay={<PopoverComponent id={blog.id} />}
+                >
+                  <td>
+                    <a href='#'>{blog.id}</a>
+                  </td>
+                </OverlayTrigger>
+                <td>{blog.title}</td>
+                <td>{blog.author}</td>
+                <td>{blog.content}</td>
+
+                <td>
+                  <Button variant='warning' onClick={() => handleEditBlog(blog)} className='mb-3'>
+                    Edit
+                  </Button>
+                  &nbsp;&nbsp;&nbsp;
+                  <Button variant='danger' onClick={() => handleDelete(blog)}>
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <UsersPagination
+        totalPages={totalPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <BlogCreateModal
+        isOpenCreateModal={isOpenCreateModal}
+        setIsOpenCreateModal={setIsOpenCreateModal}
+      />
+
+      <BlogEditModal
+        isOpenUpdateModal={isOpenUpdateModal}
+        setIsOpenUpdateModal={setIsOpenUpdateModal}
+        dataBlog={dataBlog}
+      />
+
+      <BlogDeleteModal
+        dataBlog={dataBlog}
+        isOpenDeleteModal={isOpenDeleteModal}
+        setIsOpenDeleteModal={setIsOpenDeleteModal}
+      />
+    </>
+  );
 }
 
 export default BlogsTable;
